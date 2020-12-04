396014edf62f22897e1dbb31c1f34c57ae8545ee
Dhruv Kapur <dhruvkapur4@gmail.com>
2020-12-04 18:05:14 +0530

    Added help message

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ require('dotenv').config()
 
 const client = new Discord.Client();
 
+const helpMessage =
+    `Hey trainer! Here is how you call me:
+Usage: pb <Pokemon1> <Pokemon2> ...`
+
 client.on('ready', () => {
     console.log(`PokeBot active as ${client.user.tag}`);
 });
@@ -11,19 +15,22 @@ client.on('ready', () => {
 client.on('message', msg => {
     tokens = msg.content.split(' ');
     if (tokens[0] === 'pb') {
-        for (let i = 1; i < tokens.length; i++) {
-            pokemon = tokens[i].toLowerCase();
-            axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-                .then(res => {
-                    image = res.data.sprites.other['official-artwork'].front_default;
-                    msg.react('👌');
-                    msg.channel.send(image);
-                })
-                .catch(err => {
-                    msg.react('🖕');
-                    msg.reply("Can't you spell the pokemon's name you dumbass?")
-                })
-        }
+        if (tokens.length === 1)
+            msg.reply(helpMessage);
+        else
+            for (let i = 1; i < tokens.length; i++) {
+                pokemon = tokens[i].toLowerCase();
+                axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+                    .then(res => {
+                        image = res.data.sprites.other['official-artwork'].front_default;
+                        msg.react('👌');
+                        msg.channel.send(image);
+                    })
+                    .catch(err => {
+                        msg.react('🖕');
+                        msg.reply("Can't you spell the pokemon's name you dumbass?")
+                    })
+            }
     }
 });
 
